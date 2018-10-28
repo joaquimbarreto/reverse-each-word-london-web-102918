2015e84cae3e257afbbf63679467d83097ee9412
Joaquim Barreto <joaquim@barreto.co>
2018-10-28 19:08:54 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,10 @@
-def reverse_each_word(phrase)
-  phrase.each_chars.reverse.join(" ")
+def reverse_each_word(string)
+ string = string.split('')
+ reversed_string = []
+ 
+ string.each do |char|
+  reversed_string.unshift(char)
+ end
+ 
+ return reversed_string.join('')
 end
